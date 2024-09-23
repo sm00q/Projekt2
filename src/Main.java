0a506d9e5b29409095b830481cf2b33ecbae693c
sm00q <filipplachecki2204@gmail.com>
2024-09-23 12:13:15 +0200

    added a string

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
             System.out.println("123");
+            System.out.println("4");
         }
     }
 }
